9777c39835a50d6e1644fab7a6a4d0adab886c33
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-04-15 17:26:04 +0300

    added 4u server
    Co-Authored-By: N1kO23 <59277828+N1kO23@users.noreply.github.com>

--- src/main/java/net/fabricmc/example/Xornet.java
+++ src/main/java/net/fabricmc/example/Xornet.java
@@ -7,19 +7,27 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.item.*;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
@@ -27,18 +35,42 @@ import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 
-class FrostEnchantment extends Enchantment {
-	public FrostEnchantment() {
+class ZeusEnchantment extends Enchantment {
+
+	public ZeusEnchantment() {
 		super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
 	}
-
 	@Override
 	public void onTargetDamaged(LivingEntity user, Entity target, int level) {
 		if(target instanceof LivingEntity) {
-			((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 2 * level, level - 1));
-		}
+			LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, target.world);
+			lightning.setPos(target.getX(), target.getY(), target.getZ());
+			lightning.tick();
+		};
 
 		super.onTargetDamaged(user, target, level);
+	}
+}
+
+class Server4UBlock extends Block {
+	public Server4UBlock(Settings settings) {
+		super(settings);
+		setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+		float reduceVoxelSize = (1f/16f);
+		return VoxelShapes.cuboid(reduceVoxelSize, 0f, reduceVoxelSize, 1f - reduceVoxelSize, 0.5f, 1f - reduceVoxelSize);
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+		stateManager.add(Properties.HORIZONTAL_FACING);
+	}
+
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
 	}
 }
 
@@ -55,7 +87,14 @@ public class Xornet implements ModInitializer {
 	// Create CPU items
 	public static final Item BASTEL_PROCESSOR_L1 = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(1));
 	public static final Item RAZOR_PROCESSOR_R1 = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(1));
+	public static final Item MEMORY_1VB = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(16));
+	public static final Item MEMORY_4VB = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(16));
+	public static final Item MEMORY_16VB = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(16));
 
+	// Servers
+	public static final Block SERVER_4U = new Server4UBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f));
+	public static final BlockItem SERVER_4U_BLOCKITEM = new BlockItem(SERVER_4U, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+	
 	// Overworld Ores
 	public static final Block SILICON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3.0f));
 	public static final BlockItem SILICON_ORE_BLOCKITEM = new BlockItem(SILICON_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
@@ -101,6 +140,10 @@ public class Xornet implements ModInitializer {
 					stacks.add(new ItemStack(AURA_QUARTZ));
 					stacks.add(new ItemStack(BASTEL_PROCESSOR_L1));
 					stacks.add(new ItemStack(RAZOR_PROCESSOR_R1));
+					stacks.add(new ItemStack(MEMORY_1VB));
+					stacks.add(new ItemStack(MEMORY_4VB));
+					stacks.add(new ItemStack(MEMORY_16VB));
+					stacks.add(new ItemStack(SERVER_4U_BLOCKITEM));
 				})
 				.build();
 
@@ -118,6 +161,10 @@ public class Xornet implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "silicon_shovel"),  SILICON_SHOVEL);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bastel_processor_l1"),     BASTEL_PROCESSOR_L1);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "razor_processor_r1"),  RAZOR_PROCESSOR_R1);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "memory_1vb"),  MEMORY_1VB);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "memory_4vb"),  MEMORY_4VB);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "memory_16vb"),  MEMORY_16VB);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "server_4u"),  SERVER_4U_BLOCKITEM);
 
 		// Overworld Items
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "silicon_ore"), SILICON_ORE_BLOCKITEM);
@@ -130,11 +177,11 @@ public class Xornet implements ModInitializer {
 
 		// BLOCKS ------------------------------------------------------------------------------------------
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "silicon_ore"), SILICON_ORE);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "server_4u"),  SERVER_4U);
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "nether_aura_quartz_ore"), NETHER_AURA_QUARTZ_ORE);
 
-
 		// ENCHANTMENTS ------------------------------------------------------------------------------------------
-		Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "frost"), new FrostEnchantment());
+		Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "zeus"), new ZeusEnchantment());
 
 		// Register overworld ore biome modifications!
 		RegistryKey<ConfiguredFeature<?, ?>> oreSiliconOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(MOD_ID, "ore_silicon_overworld"));
